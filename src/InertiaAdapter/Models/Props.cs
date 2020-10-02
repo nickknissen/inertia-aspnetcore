1e5da0524bade16d87cfe274f820e0307759cc43
Nick Nissen <nick.nissen@uptimedevelopment.dk>
2020-10-02 23:22:52 +0200

    Add errors to props

--- src/InertiaAdapter/Models/Props.cs
+++ src/InertiaAdapter/Models/Props.cs
@@ -5,5 +5,6 @@
         public object? Controller { get; set; }
         public object? Share { get; set; }
         public object? With { get; set; }
+        public IDictionary<string, string> Errors { get; set; } = new Dictionary<string, string>();
     }
 }
