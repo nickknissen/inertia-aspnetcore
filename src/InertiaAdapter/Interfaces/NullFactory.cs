f63b76c93470bbb3d90dd9107524d45febe4eba4
AndySong <andylauszp@gmail.com>
2021-01-18 23:33:18 +1300

    Update NullFactory.cs

--- src/InertiaAdapter/Interfaces/NullFactory.cs
+++ src/InertiaAdapter/Interfaces/NullFactory.cs
@@ -1,12 +1,19 @@
-﻿using InertiaAdapter.Core;
-using Microsoft.AspNetCore.Html;
 using System;
+using System.Collections.Generic;
+using InertiaAdapter.Core;
+using Microsoft.AspNetCore.Html;
 
 namespace InertiaAdapter.Interfaces
 {
     internal class NullFactory : IResultFactory
     {
-        public object? Share { get; set; }
+        public void Share(string key, object obj) => throw new NotImplementedException();
+
+        public void Share(string key, Func<object> func) => throw new NotImplementedException();
+
+        public Dictionary<string, object> GetShared() => throw new NotImplementedException();
+
+        public object GetSharedByKey(string key) => throw new NotImplementedException();
 
         public void SetRootView(string s) => throw new NotImplementedException();
 
